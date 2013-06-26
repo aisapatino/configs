6762e0c1e174b10f4d6a1edc97027437d5c68a84
Aisa <aisapatino@gmail.com>
2013-06-26 12:45:17 -0700

    add overwrite flag to copies

--- install - windows.bat
+++ install - windows.bat
@@ -1,6 +1,7 @@
 
-COPY .vimrc C:\Users\aisa\
-COPY aisa.vim C:\Users\aisa\vimfiles\colors
-COPY fl-gitconfig "C:\Program Files (x86)\World of Warcraft\interface\addons\flamingloot\.git\config"
-COPY flml-gitconfig "C:\Program Files (x86)\World of Warcraft\interface\addons\flaminglootsys\.git\config"
+COPY /Y .vimrc C:\Users\aisa\
+COPY /Y .gitconfig C:\Users\aisa\
+COPY /Y aisa.vim C:\Users\aisa\vimfiles\colors
+COPY /Y fl-gitconfig "C:\Program Files (x86)\World of Warcraft\interface\addons\flamingloot\.git\config"
+COPY /Y flml-gitconfig "C:\Program Files (x86)\World of Warcraft\interface\addons\flaminglootsys\.git\config"
 
