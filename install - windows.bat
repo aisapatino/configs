8b1c45fd85d35daa36302d010793c2ba9f4a4a51
unknown <aisapatino@gmail.com>
2013-06-28 15:38:32 -0700

    copy bundle folder

--- install - windows.bat
+++ install - windows.bat
@@ -4,4 +4,5 @@ COPY /Y .gitconfig C:\Users\aisa\
 COPY /Y aisa.vim C:\Users\aisa\vimfiles\colors
 COPY /Y fl-gitconfig "C:\Program Files (x86)\World of Warcraft\interface\addons\flamingloot\.git\config"
 COPY /Y flml-gitconfig "C:\Program Files (x86)\World of Warcraft\interface\addons\flaminglootsys\.git\config"
+XCOPY /Y bundle C:\Users\aisa\vimfiles /s /e
 
