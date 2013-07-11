ecafab1739aa23557654ab30d952b8cdaa615eb6
aisa <aisapatino@gmail.com>
2013-07-11 11:59:22 -0700

    also indicate current branch for each

--- check_statuses.sh
+++ check_statuses.sh
@@ -8,37 +8,45 @@ purple="\033[00;35m"
 cyan="\033[00;36m"
 reset="\033[00m"
 
+# The $? holds the exit status of the previously executed command. 0 = success
 echo 'Checking git repos...'
 cd ~/Devel
 for d in 'configs' 'flamingloot' 'flamingloot-ml' 'sjfnw' 'tracker'
 do
 	cd $d
 	OUT="\033[1m$d\033[0m"
-	RES=$(git status | grep -E '^# (Untracked|Changes|Your branch)')
-	grep -e 'Untracked' <<<${RES} >/dev/null 2>&1
-	if [ $? -eq 0 ] ; then
+	RES=$(git status | grep -E '^# (On branch|Untracked|Changes|Your branch)')
+	SECTION=$( echo "$RES" | grep -o "On branch \([a-z]\+\)")
+	SECTION=$( echo "$SECTION" | grep -o "[a-z_/\-]\+$")
+	OUT="$OUT - $SECTION"
+	SECTION=""
+
+	SECTION=$(echo "$RES" | grep -e 'Untracked')
+	if [ "$SECTION" != "" ] ; then
 		OUT="$OUT $red[Untracked]$reset"
 	fi
-	grep -e 'Changes not staged for commit' <<<${RES} >/dev/null 2>&1
-	if [ $? -eq 0 ] ; then
+	SECTION=$(echo "$RES" | grep -e 'Changes not staged for commit')
+	if [ "$SECTION" != "" ] ; then
 		OUT="$OUT $red[Modified]$reset"
 	fi
-	grep -e 'Changes to be committed' <<<${RES} >/dev/null 2>&1
-	if [ $? -eq 0 ] ; then
+	SECTION=$(echo "$RES" | grep -e 'Changes to be committed')
+	if [ "$SECTION" != "" ] ; then
 		OUT="$OUT $green[Staged]$reset"
 	fi
-	grep -e 'Your branch is ahead' <<<${RES} >/dev/null 2>&1
-	if [ $? -eq 0 ] ; then
+	SECTION=$(echo "$RES" | grep -e 'Your branch is ahead')
+	if [ "$SECTION" != "" ] ; then
 		OUT="$OUT $yellow[Ahead of remote]$reset"
 	fi
-	grep -e 'Your branch is behind' <<<${RES} >/dev/null 2>&1
-	if [ $? -eq 0 ] ; then
+	SECTION=$(echo "$RES" | grep -e 'Your branch is behind')
+	if [ "$SECTION" != "" ] ; then
 		OUT="$OUT $cyan[Behind remote]$reset"
 	fi
-	if [ "$OUT" = "\033[1m$d\033[0m" ] ; then
+
+	SECTION=$(echo "$OUT" | grep -o "\]")
+	if [ "$SECTION" = "" ] ; then
 		OUT="$OUT $green[up to date]$reset"
 	fi
-	echo -e $OUT
+	echo -e "$OUT"
 	cd ..
 done
 
