8b7b70ebe8975dbea46f4f2847057c230404a1da
unknown <aisapatino@gmail.com>
2013-09-12 01:56:04 -0700

    added global gitignore for pyc. updated windows copying script

--- install - windows.bat
+++ install - windows.bat
@@ -1,6 +1,7 @@
 REM copy over single files. /Y allows overwrite.
 COPY /Y .vimrc C:\Users\aisa\
 COPY /Y .gitconfig C:\Users\aisa\
+COPY /Y .gitignore C:\Users\aisa\
 COPY /Y aisa.vim C:\Users\aisa\vimfiles\colors
 COPY /Y fl-gitconfig "C:\Program Files (x86)\World of Warcraft\interface\addons\flamingloot\.git\config"
 COPY /Y flml-gitconfig "C:\Program Files (x86)\World of Warcraft\interface\addons\flaminglootsys\.git\config"
