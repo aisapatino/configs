f812ed60282e79c91a079d52ad6d5d01f56b94bf
Aisa <aisapatino@gmail.com>
2014-01-26 22:46:48 -0800

    conditionals for folders that might be missing

--- install - linux.sh
+++ install - linux.sh
@@ -3,19 +3,29 @@
 # git
 ln -s -f -v ~/Projects/configs/git/.gitignore ~/.gitignore
 ln -s -f -v ~/Projects/configs/git/.gitconfig ~/.gitconfig
-ln -s -f -v ~/Projects/configs/git/fl-gitconfig ~/Projects/flamingloot/.git/config
-ln -s -f -v ~/Projects/configs/git/flml-gitconfig ~/Projects/flaminglootsys/.git/config
+if [ -d /home/aisa/Projects/flamingloot ] ; then
+  ln -s -f -v ~/Projects/configs/git/fl-gitconfig ~/Projects/flamingloot/.git/config
+fi
+if [ -d ~/Projects/flaminglootsys ] ; then
+  ln -s -f -v ~/Projects/configs/git/flml-gitconfig ~/Projects/flaminglootsys/.git/config
+fi
 ln -s -f -v ~/Projects/configs/git/.gitk ~/.gitk
 
 # mysql
-ln -s -f -v ~/Projects/configs/mysql/my.cnf /etc/
+sudo ln -s -f -v ~/Projects/configs/mysql/my.cnf /etc/
 
 # terminal
 ln -s -f -v ~/Projects/configs/terminal/.bashrc ~/.bashrc
+if [ ! -d ~/.config/terminator ] ; then
+  mkdir -v ~/.config/terminator
+fi
 ln -s -f -v ~/Projects/configs/terminal/terminator-config ~/.config/terminator/config
 
 # vim
 ln -s -f -v ~/Projects/configs/vim/.vimrc ~/.vimrc
+if [ ! -d ~/.vim/colors ] ; then
+  mkdir -v ~/.vim/colors
+fi
 ln -s -f -v ~/Projects/configs/vim/aisa.vim ~/.vim/colors/aisa.vim
 ln -s -f -v ~/Projects/configs/vim/aisadark.vim ~/.vim/colors/aisadark.vim
 ln -s -f -v ~/Projects/configs/vim/bundle ~/.vim/
