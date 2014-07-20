099a42774905bca104d0383c447199f70e1ca8e8
Aisa <aisapatino@gmail.com>
2014-07-20 12:21:54 -0700

    update script to work with vundle
    create bundle dir if needed
    git clone vundle repo into it if needed

--- install - linux.sh
+++ install - linux.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+# Create needed directiories and symbolic links for config files
+
+# -v verbose (write actions to stdout)
+#
+# ln:
+#   -s symbolic link
+#   -f override existing
+#
+# mkdir:
+#   -p no error if existing, make parent dirs as needed
+
 # git
+echo "--- GIT ---"
 ln -s -f -v ~/Projects/configs/git/.gitignore ~/.gitignore
 ln -s -f -v ~/Projects/configs/git/.gitconfig ~/.gitconfig
 if [ -d /home/aisa/Projects/flamingloot ] ; then
@@ -12,9 +24,11 @@ fi
 ln -s -f -v ~/Projects/configs/git/.gitk ~/.gitk
 
 # mysql
+echo "--- MYSQL ---"
 sudo ln -s -f -v ~/Projects/configs/mysql/my.cnf /etc/
 
 # terminal
+echo "--- TERMINAL ---"
 ln -s -f -v ~/Projects/configs/terminal/.bashrc ~/.bashrc
 if [ ! -d ~/.config/terminator ] ; then
   mkdir -v ~/.config/terminator
@@ -22,13 +36,27 @@ fi
 ln -s -f -v ~/Projects/configs/terminal/terminator-config ~/.config/terminator/config
 
 # vim
+echo "--- VIM ---"
 ln -s -f -v ~/Projects/configs/vim/.vimrc ~/.vimrc
+
 mkdir -p -v ~/.vim/colors
 ln -s -f -v ~/Projects/configs/vim/aisa.vim ~/.vim/colors/aisa.vim
 ln -s -f -v ~/Projects/configs/vim/aisadark.vim ~/.vim/colors/aisadark.vim
-ln -s -f -v ~/Projects/configs/vim/bundle ~/.vim/
+
+if [ ! -d ~/.vim/bundle ] ; then
+  mkdir -v ~/.vim/bundle
+fi
+if [ ! -d ~/.vim/bundle/Vundle.vim ] ; then
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
 ln -s -f -v ~/Projects/configs/vim/custom-syntax ~/.vim/
 ln -s -f -v ~/Projects/configs/vim/snippets ~/.vim/
 ln -s -f -v ~/Projects/configs/vim/CustomGrepFileAssoc ~/.vim/CustomGrepFileAssoc
+
+# linters
+echo "--- LINTERS ---"
 ln -s -f -v ~/Projects/configs/vim/.pylintrc ~/.pylintrc
+
+echo "\nMake sure to run :BundleInstall once you open vim"
 
