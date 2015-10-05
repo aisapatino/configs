a0e1aa49dd9677ab78fff75627452b3d7987ae96
Anna Luisa Patiño West <anna.luisa.west@formidablelabs.com>
2015-10-05 11:58:33 -0700

    combine 3 bash install scripts

--- install/base.sh
+++ install/base.sh
@@ -2,42 +2,67 @@
 
 # Create needed directiories and symbolic links for config files
 
-# general: -v verbose (write actions to stdout)
-# ln: -s symbolic link
-#     -f override existing
+# ln: -s symbolic link, -f override existing
 # mkdir: -p no error if existing, make parent dirs as needed
 
-HEADER="\n*** %s ***\n"
+header="\n*** %s ***\n"
+configs=~/Projects/configs
 
-printf "$HEADER" "GIT"
-ln -s -f -v ~/Projects/configs/git/.gitignore ~/.gitignore
-ln -s -f -v ~/Projects/configs/git/.gitconfig ~/.gitconfig
-ln -s -f -v ~/Projects/configs/git/.gitk ~/.gitk
+if [[ $1 == m* ]] || [[ $1 == w* ]] ; then # "mac" or "work"
+  install_type='work'
+elif [[ $1 == l* ]] || [[ $1 == h* ]] ; then # "linux" or "home"
+  install_type='linux'
+else
+  install_type='base'
+fi
+
+echo -e "Running $install_type setup\n"
+
+printf "$header" "GIT"
+
+ln -s -f -v "$configs/git/.gitignore ~/.gitignore"
+ln -s -f -v "$configs/git/.gitconfig ~/.gitconfig"
+ln -s -f -v "$configs/git/.gitk ~/.gitk"
 
 if [ ! -f ~/.git-completion.bash ]; then
   echo "Getting git-completion.bash"
-  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+  curl -o ~/.git-completion.bash \
+    https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 fi
 
-printf "$HEADER" "BASH"
-# links base bashrc. will presumably be overridden in specific install script
-ln -s -f -v ~/Projects/configs/terminal/.bashrc-base ~/.bashrc
+printf "$header" "SHELL/TERMINAL"
 
-printf "$HEADER" "VIM"
-ln -s -f -v ~/Projects/configs/vim/.vimrc ~/.vimrc
-ln -s -f -v ~/Projects/configs/vim/.gvimrc ~/.gvimrc
+if [ "$install_type" = "work" ] ; then
+  ln -s -f -v "$configs/terminal/.bashrc-work ~/.bashrc"
+elif [ "$install_type" = "linux" ] ; then
+  ln -s -f -v "$configs/terminal/.bashrc-linux ~/.bashrc"
+  mkdir -v -p ~/.config/terminator
+  ln -s -f -v "$configs/terminal/terminator-config ~/.config/terminator/config"
+else
+  ln -s -f -v "$configs/terminal/.bashrc-base ~/.bashrc"
+fi
 
-mkdir -p -v ~/.vim/colors
-ln -s -f -v ~/Projects/configs/vim/colors/aisa.vim ~/.vim/colors/aisa.vim
-ln -s -f -v ~/Projects/configs/vim/colors/aisadark.vim ~/.vim/colors/aisadark.vim
+if [ $install_type = "linux" ] ; then
+  printf "$HEADER" "LINTERS"
+  ln -s -f -v "$configs/linters/.pylintrc ~/.pylintrc"
+  ln -s -f -v "$configs/linters/.eslintrc ~/.eslintrc"
+fi
+
+printf "$header" "VIM"
+
+ln -s -f -v "$configs/vim/.vimrc ~/.vimrc"
+ln -s -f -v "$configs/vim/.gvimrc ~/.gvimrc"
+ln -s -f -v "$configs/vim/colors ~/.vim/"
 
 if [ ! -f ~/.vim/autoload/plug.vim ] ; then
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  echo "Downloading vim-plug"
+  curl -Lo ~/.vim/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-ln -s -f -v ~/Projects/configs/vim/custom-syntax ~/.vim/
-ln -s -f -v ~/Projects/configs/vim/custom-snippets ~/.vim/
-ln -s -f -v ~/Projects/configs/vim/CustomGrepFileAssoc ~/.vim/CustomGrepFileAssoc
+ln -s -f -v "$configs/vim/custom-after ~/.vim/"
+ln -s -f -v "$configs/vim/custom-snippets ~/.vim/"
+ln -s -f -v "$configs/vim/CustomGrepFileAssoc ~/.vim/"
+ln -s -f -v "$configs/vim/filetype.vim ~/.vim/"
 
-printf "$HEADER" "Make sure to run :PlugInstall once you open vim"
+printf "$header" "Make sure to run :PlugInstall once you open vim"
--- install/work.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Runs base.sh for base config
-# This file contains mac/work specific things
-
-bash ~/Projects/configs/install/base.sh
-
-printf "\n*** %s ***\n" "BASH"
-ln -s -f -v ~/Projects/configs/terminal/.bashrc-work ~/.bashrc
